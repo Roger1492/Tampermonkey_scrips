8bc477b4d1e102733da2ce4bfb914d5f1464bfb9
Roger1492 <rogershen1492@gmail.com>
2018-06-18 02:51:13 +0800

    javbus2 v0.2

--- javbus2图片排列器.js
+++ javbus2图片排列器.js
@@ -2,7 +2,7 @@
 // @name         Javbus2 图片排列器(重制版)
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  页面一分为二，做些缩略提，右侧详细信息
+// @description  页面一分为二，做些缩略提，右侧详细信息(最好在窗口最大化的情况下查阅)
 // @author       You
 // @match        https://www.javbus2.pw/series/*
 // @match        https://www.javbus2.pw/star/*
@@ -10,7 +10,7 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     let ad = document.querySelectorAll(".ad-table, .ad-list");
@@ -19,35 +19,35 @@
     let createSection = document.createElement("section");
     let createImgBig = document.createElement("img");
     let oneImg = document.getElementsByClassName("one");
-    
-    
+
+
     // 先删除所有的广告
-    for(let i = 0; i < ad.length; i++){
+    for (let i = 0; i < ad.length; i++) {
         ad[i].parentElement.removeChild(ad[i]);
     }
-    
-    // TODO: 1 点击查看按钮后，如果在底部，则返回到顶部
-    // TODO: 2  番号的获取还有些问题，目前只能获取4的字母开头的。Regexp还有些问题。  
 
     // 删除所有item的内联style
-    for(let i = 0; i < item.length; i++){
+    for (let i = 0; i < item.length; i++) {
         item[i].removeAttribute("style");
         item[i].style.height = "400px";     // 所有的item的高度都设置为相同的
 
         // 给每一个item添加一个查看按钮，
         let createSpan = document.createElement("span");
-        createSpan.style.position =  "absolute";
+        createSpan.style.position = "relative";
+        createSpan.style.transform = "translate(127px, -28px)";
         createSpan.innerHTML = "查看";
         createSpan.style.display = "inline-block";
         createSpan.style.width = "50px";
         createSpan.style.zIndex = "99999";
         createSpan.style.background = "gray";
-        createSpan.style.fontSize = "24px";
-        createSpan.style.color = "whtie";
+        createSpan.style.color = "white";
+        createSpan.style.fontSize = "14px";
+        createSpan.style.lineHeight = "16px";
+        createSpan.style.textAlign = "center";
         createSpan.style.cursor = "pointer";
         item[i].appendChild(createSpan);
-        createSpan.addEventListener("click", function(e){
-            // console.log(this.previousElementSibling);
+        createSpan.addEventListener("click", function (e) {
+            createSection.scrollTop = "0px";
 
             // 获取AV大图的地址：
             // 原始图片：https://pics.javcdn.pw/thumb/6bgm.jpg
@@ -58,30 +58,26 @@
 
             // 获取AV番号
             let avBango = this.previousElementSibling.querySelector("date").innerHTML;
-            // let avLetter = /[a-z]{1,6}/g.exec(avBango);
-            // let avNum = /\d{3}/g.exec(avBango)[0];
-            let avLetter = avBango.substr(0,4);
-            let avNum = avBango.substr(5,3);
-            // console.log(avLetter + " - " + avNum);
-            let bango = avLetter.toLocaleLowerCase() + "00" + avNum; 
+            let avLetter = avBango.split("-")[0];
+            let avNum = avBango.split("-")[1];
+            let bango = avLetter.toLocaleLowerCase() + "00" + avNum;
 
-           
             // 创建图片容器
             createImgBig.src = add_img2;
 
             // 赋值图片地址
-            for(let i = 1; i <= oneImg.length; i++){
-                oneImg[i].src = "https://pics.dmm.co.jp/digital/video/" + bango + "/" + bango + "jp-" + i + ".jpg";
+            for (let i = 0; i <= oneImg.length; i++) {
+                oneImg[i].src = "https://pics.dmm.co.jp/digital/video/" + bango + "/" + bango + "jp-" + (i + 1) + ".jpg";
             }
-            
+
         }, false);
     }
     waterfall.removeAttribute("style");
-    
+
     // 所有item放置到左半边
     waterfall.style.margin = "10px";
-    waterfall.style.width = "980px";
-    
+    waterfall.style.width = "960px";
+
     // 增加有半边的详细信息查看框
     createSection.setAttribute("id", "detail");
     createSection.style.position = "fixed";
@@ -95,7 +91,7 @@
     createSection.appendChild(createImgBig);
 
     //创建10个img标签
-    for(let i = 0; i < 10; i++){
+    for (let i = 0; i < 10; i++) {
         let imgs = document.createElement("img");
         imgs.classList.add("one");
         createSection.appendChild(imgs);
